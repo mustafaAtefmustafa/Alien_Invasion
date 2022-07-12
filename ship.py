9e06ddd72676f48661e6166388e09056e3491f60
mustafaAtefmustafa <mustafaatef761@gmail.com>
2022-07-12 17:33:22 +0200

    adjusting the speed and limiting the range of ship movement

--- ship.py
+++ ship.py
@@ -8,6 +8,8 @@ class Ship():
         """Initialize ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
         # Movement flags
         self.moving_right = False
         self.moving_left = False
@@ -21,17 +23,24 @@ class Ship():
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
     def update(self):
         """Update the ship's position based on movement flag."""
-        if self.moving_right == True:
-            self.rect.x += 1
-        if self.moving_left == True:
-            self.rect.x -= 1
-        if self.moving_up == True:
-            self.rect.y -= 1
-        if self.moving_down == True:
-            self.rect.y += 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        # Update rect object from self.x and self.y
+        self.rect.x = self.x
+        self.rect.y = self.y
+
 
     def blitme(self):
         """Draw the ship at its current location."""

b8d3b00445a08b93d7ca29f714465de1dffcf289
mustafaAtefmustafa <mustafaatef761@gmail.com>
2022-07-12 20:13:32 +0200

    Firing bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
-from distutils.log import FATAL
 import sys
 import pygame as p
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -16,6 +16,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         p.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = p.sprite.Group()
+
 
     def run_game(self):
         """"Start the main loop for the game"""
@@ -23,6 +25,7 @@ class AlienInvasion:
             # Watch for keyboard and mouse events
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
 
@@ -52,22 +55,45 @@ class AlienInvasion:
             self.ship.moving_down = True
         elif event.key == p.K_q:
             sys.exit()
+        elif event.key == p.K_SPACE:
+            self._fire_bullet()
+
 
     def _check_keyup_events(self, event):
         """Responds to key releases."""
         if event.key == p.K_RIGHT:
             self.ship.moving_right = False
-        elif event.key ==p.K_LEFT:
+        elif event.key == p.K_LEFT:
             self.ship.moving_left = False
-        elif event.key ==p.K_UP:
+        elif event.key == p.K_UP:
             self.ship.moving_up = False
-        elif event.key ==p.K_DOWN:
+        elif event.key == p.K_DOWN:
             self.ship.moving_down = False
+
+    def _fire_bullet(self):
+        """Creat new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
+
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         p.display.flip()
 
 
